c166b9de7405f60e3e9ca8307d05e87e4b2c2122
CrankyCat-Loves-Coding <crankycatlovecoding@outlook.com>
2022-07-30 20:20:23 +0000

    add sort by price and size functionality

--- products/models.py
+++ products/models.py
@@ -37,6 +37,7 @@ class Product(models.Model):
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
+    size = models.CharField(max_length=254, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=6,
         decimal_places=2,
--- products/views.py
+++ products/views.py
@@ -17,15 +17,33 @@ def all_products(request):
     query = None
     # search start with none to avoid error
     categories = None
+    sort = None
+    direction = None
 
     if request.GET:
+        # search by price or size
+        if 'sort' in request.GET:
+            sortkey = request.GET['sort']
+            sort = sortkey
+            if sortkey == 'name':
+                sortkey = 'lower_name'
+                products = products.annotate(lower_name=Lower('name'))
+
+            if 'direction' in request.GET:
+                direction = request.GET['direction']
+                if direction == 'desc':
+                    sortkey = f'-{sortkey}'
+            products = products.order_by(sortkey)
+
+    if request.GET:
+        # search by category
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
             categories = Category.objects.filter(name__in=categories)
 
     if request.GET:
-        # search functionality
+        # search by keywords
         if 'q' in request.GET:
             query = request.GET['q']
             if not query:
@@ -39,10 +57,13 @@ def all_products(request):
                 name__icontains=query) | Q(description__icontains=query)
             products = products.filter(queries)
 
+    current_sorting = f'{sort}_{direction}'
+
     context = {
         'products': products,
         'search_term': query,
         'current_categories': categories,
+        'current_sorting': current_sorting,
     }
 
     return render(request, 'products/products.html', context)
