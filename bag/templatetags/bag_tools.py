8f55365d2c759625696cec25112829717843c91a
CrankyCat-Loves-Coding <crankycatlovecoding@outlook.com>
2022-08-25 21:20:45 +0000

    fix bag subtotal

---
+++ bag/templatetags/bag_tools.py
@@ -0,0 +1,11 @@
+"""calulate subtotal """
+from django import template
+
+
+register = template.Library()
+
+
+@register.filter(name='calc_subtotal')
+def calc_subtotal(price, quantity):
+    """calulate subtotal in the bag"""
+    return price * quantity
