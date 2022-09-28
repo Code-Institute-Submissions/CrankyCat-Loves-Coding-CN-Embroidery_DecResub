da538f3efa63bd27e3b5d4ae57ee1d2b2c2bd195
CrankyCat-Love-Coding <crankycatlovecoding@outlook.com>
2022-09-28 14:22:07 +0000

    adjuated profile page added user's name to it

--- comment/admin.py
+++ comment/admin.py
@@ -1,10 +1,1 @@
 from django.contrib import admin
-from .models import Comment
-
-
-class CommentAdmin(admin.ModelAdmin):
-    '''register Comment app to admin site'''
-    list_display = ['like']
-
-
-admin.site.register(Comment, CommentAdmin)
