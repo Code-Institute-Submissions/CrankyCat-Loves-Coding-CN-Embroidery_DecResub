2fee8c4fc436975df9bc5078d77852771b300204
CrankyCat-Love-Coding <crankycatlovecoding@outlook.com>
2022-10-28 22:30:46 +0000

    added pagination to event page

--- comment/views.py
+++ comment/views.py
@@ -7,6 +7,7 @@ from django.views.generic import ListView
 class EventView(ListView):
     template_name = "comment/event.html"
     context_object_name = "event_list"
+    paginate_by = 3
 
     def get_queryset(self):
         event_list = Event.objects.filter(status='p') 
